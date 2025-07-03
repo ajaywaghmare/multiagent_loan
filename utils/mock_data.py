e77d791776788cf25a6a11bb2df73ecad919754e
Ajay Waghmare <awag0001@student.monash.edu>
2025-07-03 12:08:37 +1000

    Added changes in mock_data.py

--- utils/mock_data.py
+++ utils/mock_data.py
@@ -1,18 +1,3 @@
-# def get_customer_profile(customer_id):
-#     return {
-#         "customer_id": customer_id,
-#         "name": "John Doe",
-#         "age": 35,
-#         "email": "johndoe@example.com",
-#         "phone": "555-1234",
-#         "address": "123 Main Street, Anytown, USA",
-#         "credit_score": 300,
-#         "income": 10000,
-#         "employment_status": "Employed",
-#         "loan_amount_request": 75000
-#     }
-
-
 import json
 
 def get_customer_profile(customer_id):
